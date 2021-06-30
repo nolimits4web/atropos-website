ab1cc33f7a0048d70ec3758cf0d73b508b13ebee
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-06-30 19:50:41 +0300

    remove forks

--- src/components/GithubStats.js
+++ src/components/GithubStats.js
@@ -4,7 +4,6 @@ import { GithubIcon } from './GithubIcon';
 function getLocalStats() {
   return {
     stars: localStorage.getItem('mariko-git-stats-stars'),
-    forks: localStorage.getItem('mariko-git-stats-forks'),
   };
 }
 
@@ -13,8 +12,8 @@ async function fetchGitStats(local) {
     return getLocalStats();
   }
   const res = await fetch('https://api.github.com/repos/nolimits4web/mariko');
-  const { stargazers_count, forks } = await res.json();
-  if (stargazers_count || forks) {
+  const { stargazers_count } = await res.json();
+  if (stargazers_count) {
     localStorage.setItem('mariko-git-stats-date', new Date().getTime());
   }
   if (stargazers_count) {
@@ -23,23 +22,18 @@ async function fetchGitStats(local) {
       stargazers_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','),
     );
   }
-  if (forks || forks === 0) {
-    localStorage.setItem(
-      'mariko-git-stats-forks',
-      forks.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','),
-    );
-  }
+
   return getLocalStats();
 }
 
 export const GithubStats = () => {
-  const [data, setData] = useState({ stars: 0, forks: 0 });
+  const [data, setData] = useState({ stars: 0 });
   useEffect(() => {
     const gitStatsDate = localStorage.getItem('mariko-git-stats-date');
     const local =
       gitStatsDate && new Date().getTime() - gitStatsDate * 1 < 1000 * 60 * 60;
     fetchGitStats(local).then((res) => {
-      if (res.stars !== null && res.forks !== null) {
+      if (res.stars !== null) {
         setData(res);
       }
     });
@@ -47,10 +41,7 @@ export const GithubStats = () => {
   return (
     <div className="inline-flex items-center space-x-2">
       <GithubIcon className="inline-block" height="20" />
-      {[
-        [data.stars, 'stars'],
-        [data.forks, 'forks'],
-      ].map(([value, label]) => (
+      {[[data.stars, 'stars']].map(([value, label]) => (
         <a
           key={label}
           className="text-xs hover:opacity-50 hover:no-underline"

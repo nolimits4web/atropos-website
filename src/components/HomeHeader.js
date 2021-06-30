09281eb260e4b4cbdb8d77ad18f494f332058bfe
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-06-30 10:27:37 +0300

    0.7.1

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -6,7 +6,7 @@ export const HomeHeader = () => {
   return (
     <div className="bg-gradient-to-b from-primary to-primary-dark -mt-16 border-b-2 border-primary">
       <div className="max-w-screen-lg mx-auto pt-32 pb-16 px-4 md:px-8 lg:px-16">
-        <Mariko className="mariko-header w-full " highlight={false}>
+        <Mariko className="mariko-header w-full" highlight={false}>
           <img
             className="mariko-header-spacer"
             src="/images/header/mariko-bg.svg"

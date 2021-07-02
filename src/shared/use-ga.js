3eca48410c04ac6c144410ea61d1370793fb278e
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-07-02 12:35:32 +0300

    updage GA

--- src/shared/use-ga.js
+++ src/shared/use-ga.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export const useGA = (router) => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-QK12NJ1F28';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-HL41YE945C';
     script.onload = () => {
       window.dataLayer = window.dataLayer || [];
       // eslint-disable-next-line
@@ -12,7 +12,7 @@ export const useGA = (router) => {
         window.dataLayer.push(arguments);
       };
       window.gtag('js', new Date());
-      window.gtag('config', 'G-QK12NJ1F28');
+      window.gtag('config', 'G-HL41YE945C');
     };
     document.body.appendChild(script);
 

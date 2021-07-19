611d17e1da152b5845ed5516551ae9998ccc12ce
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-07-19 19:54:06 +0300

    add theme color

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
+          <meta name="theme-color" content="#B53DF5" />
         </Head>
         <body>
           <Main />

0ad5a3c2f070f95cece07c1bbbb5df23565ed338
Vladimir Kharlampidi <nolimits4web@gmail.com>
2023-06-23 09:54:59 +0300

    chore: fix Pre default language

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,15 +1,13 @@
 /* eslint-disable react/function-component-definition */
-import {Highlight, themes} from 'prism-react-renderer';
+import { Highlight, themes } from 'prism-react-renderer';
 // eslint-disable-next-line import/no-unresolved
-// import theme from 'prism-react-renderer/themes/dracula';
 import stripIndent from 'strip-indent';
-
 
 const removeIndent = (code = '') => {
   return stripIndent(code).trim();
 };
 
-export const Pre = ({ children, lang }) => {
+export const Pre = ({ children, lang = '' }) => {
   return (
     <Highlight
       theme={themes.dracula}
@@ -30,4 +28,3 @@ export const Pre = ({ children, lang }) => {
     </Highlight>
   );
 };
-

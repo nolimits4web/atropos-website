da9aab204933abd58114e4ed37693330aaca5059
Vladimir Kharlampidi <nolimits4web@gmail.com>
2024-12-23 13:24:16 -0500

    lint

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,39 +3,37 @@ import { GithubStats } from './GithubStats';
 
 export const Nav = ({ className }) => {
   return (
-    <>
-      <div className={className || ''}>
-        <div className="h-16 flex items-center justify-between max-w-screen-xl mx-auto px-4 md:px-8 z-10 relative text-white">
-          <div className="flex items-center space-x-2 sm:space-x-4">
+    <div className={className || ''}>
+      <div className="h-16 flex items-center justify-between max-w-screen-xl mx-auto px-4 md:px-8 z-10 relative text-white">
+        <div className="flex items-center space-x-2 sm:space-x-4">
+          <Link href="/">
+            <img
+              src="/images/logo-inner.svg"
+              className="w-16 h-16"
+              alt="Atropos"
+            />
+          </Link>
+          <div className="flex items-center">
             <Link href="/">
               <img
-                src="/images/logo-inner.svg"
-                className="w-16 h-16"
+                className="h-4 mr-2 hidden sm:block"
+                src="/images/logo-text-white.svg"
                 alt="Atropos"
               />
             </Link>
-            <div className="flex items-center">
-              <Link href="/">
-                <img
-                  className="h-4 mr-2 hidden sm:block"
-                  src="/images/logo-text-white.svg"
-                  alt="Atropos"
-                />
-              </Link>
-              <span className="text-xs font-bold sm:-mt-4">
-                v{process.env.atroposVersion}
-              </span>
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Link href="/docs/" className="font-medium hover:opacity-50">
-              <span className="sm:hidden">Docs</span>
-              <span className="hidden sm:block">Documentation</span>
-            </Link>
-            <GithubStats />
+            <span className="text-xs font-bold sm:-mt-4">
+              v{process.env.atroposVersion}
+            </span>
           </div>
         </div>
+        <div className="flex items-center space-x-4">
+          <Link href="/docs/" className="font-medium hover:opacity-50">
+            <span className="sm:hidden">Docs</span>
+            <span className="hidden sm:block">Documentation</span>
+          </Link>
+          <GithubStats />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
